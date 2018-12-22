21019cbe28512d60beded1a626d8c1e083d0501b
WillieMaddox <willie.maddox@gmail.com>
2018-12-22 11:38:42 -0600

    refactor: flip sign on z accelerations to obey NED

--- src/QuadControl.cpp
+++ src/QuadControl.cpp
@@ -180,15 +180,15 @@ float QuadControl::AltitudeControl(float posZCmd, float velZCmd, float posZ, flo
   float velZErr = (velZCmd - velZ);
   integratedAltitudeError += posZErr * dt;
   float accelZ = kpPosZ * posZErr + kpVelZ * velZErr + KiPosZ * integratedAltitudeError + accelZCmd;
-  float accelNet = (accelZ - 9.81f) / R(2, 2); // R(2, 2) -> b_z
-  accelNet = CONSTRAIN(accelNet, -maxAscentRate / dt, maxDescentRate / dt); //Flip since z points down.
-  thrust = -mass * accelNet;
+  float accelNet = (9.81f - accelZ) / R(2, 2); // R(2, 2) -> b_z
+  accelNet = CONSTRAIN(accelNet, -maxDescentRate / dt, maxAscentRate / dt);
+  thrust = mass * accelNet;
 
   // reserve some thrust margin for angle control
   float thrustMargin = .1f*(maxMotorThrust - minMotorThrust);
   thrust = CONSTRAIN(thrust, (minMotorThrust + thrustMargin) * 4.f, (maxMotorThrust - thrustMargin) * 4.f);
 
-/////////////////////////////// END STUDENT CODE ////////////////////////////
+  /////////////////////////////// END STUDENT CODE ////////////////////////////
   
   return thrust;
 }
